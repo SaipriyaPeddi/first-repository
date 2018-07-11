3b425d8e98ed3a0d3ffc60a194f6c075d9fa7e14
SaipriyaPeddi <37661361+SaipriyaPeddi@users.noreply.github.com>
2018-07-11 23:14:18 +0530

    my file

---
+++ MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.android;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		Thread t1=new Thread(){
+			
+			public void run(){
+			 
+				try{
+				 
+					int i=0;
+					while(i<2000){
+						sleep(100);
+						i=i+100;
+					}
+					Intent it=new Intent(MainActivity.this,ActivityLifeCycle.class);
+					startActivity(it);
+					finish();
+			 }
+				catch(Exception e){}
+		}
+		
+	   };
+		t1.start();
+		
+	}
+
+
+}
